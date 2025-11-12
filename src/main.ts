5eb8366241aed823d37adf51f7ebdeb505b3456f
Andrea Mejia <yamejia088@gmail.com>
2025-11-12 18:01:51 -0500

    [FEAT] Global project settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configDoc = new DocumentBuilder()
+    .setTitle('API KARU')
+    .setDescription('Documentación de la API KARU desarrollada en NestJS')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, configDoc);
+  SwaggerModule.setup('api/docs', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
