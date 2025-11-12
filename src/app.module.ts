2146b66f03a93d02abf11fdf4589b1901920bca9
Andrea Mejia <yamejia088@gmail.com>
2025-11-12 18:09:35 -0500

    [ADD] Authentication module base

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         synchronize: false,
       }),
     }),
+    AuthModule,
     // UsersModule,
   ],
   controllers: [AppController],
