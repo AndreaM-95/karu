75dbb25260c4f0cb7ff0762896a9bc925f108f5f
Andrea Mejia <yamejia088@gmail.com>
2025-11-12 18:20:08 -0500

    [ADD] Payments module base

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { VehiclesModule } from './modules/vehicles/vehicles.module';
 import { TripsModule } from './modules/trips/trips.module';
+import { PaymentsModule } from './modules/payments/payments.module';
 
 @Module({
   imports: [
@@ -29,6 +30,7 @@ import { TripsModule } from './modules/trips/trips.module';
     UsersModule,
     VehiclesModule,
     TripsModule,
+    PaymentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
