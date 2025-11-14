c1d80a7dddf3dccd714a35e96f7ee0d41898dc92
valeriavcgr <valeria3025gon@gmail.com>
2025-11-13 20:58:36 -0500

    [FEAT] UH-01 create user

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/User.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { createUserDTO } from './dto/createUser.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(@InjectRepository(User) private userRepo){}
+
+    createUser(newUser: createUserDTO){
+        const userCreated = this.userRepo.create(newUser)
+        return this.userRepo.save(userCreated), {message: "User has been created successfull"}
+    }
+}
