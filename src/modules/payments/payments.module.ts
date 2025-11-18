db41334437038fee4d624d42c2bb2486d0f9947d
Dani-02R <da933522@gmail.com>
2025-11-18 18:57:54 -0500

    [ADD] HU-05 – Admin payments summary

--- src/modules/payments/payments.module.ts
+++ src/modules/payments/payments.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
-import { Payment } from './entities/Payment.entity';
+import { Payment } from './entities/payment.entity';
+import { Trip } from '../trips/entities/trip.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [TypeOrmModule.forFeature([Payment, Trip])],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
